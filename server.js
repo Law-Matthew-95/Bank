69781ec9dbfcdc4947b9315f5d5f3f0ffce6c2e5
Matthew Law <matthew.law1995@gmail.co.uk>
2021-01-28 11:57:01 +0000

    new

--- server.js
+++ server.js
@@ -3,22 +3,22 @@ const app = express()
 const { sequelize } = require('./models')
 const { requiresAuth } = require('express-openid-connect');
 const {auth} = require('express-openid-connect')
-// const authSettings=
-// {
-//     authRequired: false,
-//     auth0Logout: true,
-//     secret: "This isn't the greatest secret in the world. This is a tribute. ",
-//     baseURL: 'https://kach1ng.herokuapp.com/',
-//     clientID: 'WV9ock0rqQZG8v1sGbWJT5OpRCfiGkQ7',
-//     issuerBaseURL: 'https://dev-81ef3zeo.eu.auth0.com'
-// }
+const authSettings=
+{
+    authRequired: false,
+    auth0Logout: true,
+    secret: "This isn't the greatest secret in the world. This is a tribute. ",
+    baseURL: 'https://kach1ng.herokuapp.com/',
+    clientID: 'WV9ock0rqQZG8v1sGbWJT5OpRCfiGkQ7',
+    issuerBaseURL: 'https://dev-81ef3zeo.eu.auth0.com'
+}
 
 if(process.env.NODE_ENV !== 'production') {
     require('dotenv').config()
   }
 
 app.use(express.json())
-// app.use(auth(authSettings)) 
+app.use(auth(authSettings)) 
 
 
 app.get('/', (req, res) => {
